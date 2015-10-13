5733ede38ad42649b498b0c980f249a1036bed84
catterer <catterer@yandex.ru>
2015-10-13 19:36:45 +0300

    multi-digit numbers

--- expr.erl
+++ expr.erl
@@ -5,21 +5,24 @@ lpri([]) -> ok;
 lpri([A|T]) -> io:format("~c~n", [A]), lpri(T).
 
 symbolType(S) ->
-    case S of
-        $+  -> {oper, plus};
-        $-  -> {oper, minus};
-        $/  -> {oper, divide};
-        $*  -> {oper, mult};
-        $   -> space;
-        _   -> val
+    if
+        S =:= $+  -> {oper, plus};
+        S =:= $-  -> {oper, minus};
+        S =:= $/  -> {oper, divide};
+        S =:= $*  -> {oper, mult};
+        S =:= $   -> space;
+        S >= 16#30, S =< 16#39  -> {val, S - 16#30}
     end.
 
-simpleParse([], Acc, Opcode) -> [Opcode|Acc];
-simpleParse([A|T], Acc, OldOpcode) ->
+addOrInc(Acc, V, Ndig) when Ndig =:= 0 -> [V|Acc];
+addOrInc([Old|Acc], V, Ndig) when Ndig > 0 -> [Old*10 + V|Acc].
+
+simpleParse([], Acc, Opcode, _) -> [Opcode|Acc];
+simpleParse([A|T], Acc, OldOpcode, Ndig) ->
     case symbolType(A) of
-        {oper, Opcode}  -> simpleParse(T, Acc, Opcode);
-        val             -> simpleParse(T, [A|Acc], OldOpcode);
-        space           -> simpleParse(T, Acc, OldOpcode)
+        {oper, Opcode}  -> simpleParse(T, Acc, Opcode, 0);
+        {val, V}        -> simpleParse(T, addOrInc(Acc, V, Ndig), OldOpcode, Ndig+1);
+        space           -> simpleParse(T, Acc, OldOpcode, Ndig)
     end.
 
-simpleParse(Str) -> list_to_tuple(simpleParse(Str, [], undefined)).
+simpleParse(Str) -> list_to_tuple(simpleParse(Str, [], undefined, 0)).
