f5789cd08442aed2f007e1804034371f29c6c775
catterer <catterer@yandex.ru>
2015-10-14 12:44:07 +0300

    calc done

--- expr.erl
+++ expr.erl
@@ -1,5 +1,5 @@
 -module(expr).
--export([lpri/1, simpleParse/1]).
+-export([lpri/1, simpleParse/1, eval/1, calc/1]).
 
 lpri([]) -> ok;
 lpri([A|T]) -> io:format("~c~n", [A]), lpri(T).
@@ -11,18 +11,35 @@ symbolType(S) ->
         S =:= $/  -> {oper, divide};
         S =:= $*  -> {oper, mult};
         S =:= $   -> space;
+        S =:= $(  -> braceOpen;
+        S =:= $)  -> braceClose;
         S >= 16#30, S =< 16#39  -> {val, S - 16#30}
     end.
 
 addOrInc(Acc, V, Ndig) when Ndig =:= 0 -> [V|Acc];
 addOrInc([Old|Acc], V, Ndig) when Ndig > 0 -> [Old*10 + V|Acc].
 
-simpleParse([], Acc, Opcode, _) -> [Opcode|Acc];
+simpleParse([], Acc, Opcode, _) -> list_to_tuple([Opcode|Acc]);
 simpleParse([A|T], Acc, OldOpcode, Ndig) ->
     case symbolType(A) of
         {oper, Opcode}  -> simpleParse(T, Acc, Opcode, 0);
-        {val, V}        -> simpleParse(T, addOrInc(Acc, V, Ndig), OldOpcode, Ndig+1);
-        space           -> simpleParse(T, Acc, OldOpcode, Ndig)
+        space           -> simpleParse(T, Acc, OldOpcode, Ndig);
+        braceOpen       ->
+            {NewTail, Parsed} = simpleParse(T, [], nil, 0),
+            simpleParse(NewTail, lists:append(Acc, [Parsed]), OldOpcode, 0);
+        braceClose      -> {T, simpleParse([], Acc, OldOpcode, 0)};
+        {val, V}        -> simpleParse(T, addOrInc(Acc, V, Ndig), OldOpcode, Ndig+1)
     end.
 
-simpleParse(Str) -> list_to_tuple(simpleParse(Str, [], undefined, 0)).
+simpleParse(Str) -> simpleParse(Str, [], nil, 0).
+
+eval(A) when is_number(A) -> A;
+eval({Action, A, B}) ->
+    case Action of
+        plus    -> eval(A) + eval(B);
+        minus   -> eval(A) - eval(B);
+        divide  -> eval(A) / eval(B);
+        mult    -> eval(A) * eval(B)
+    end.
+
+calc(Str) -> eval(simpleParse(Str)).
